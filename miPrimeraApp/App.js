498f9d9b5dd4f50c26b3b03af3f262216148494d
Matias Ezequiel Otero <matias.e.otero@accenture.com>
2021-04-11 02:40:31 -0300

    Add new screens

--- miPrimeraApp/App.js
+++ miPrimeraApp/App.js
@@ -1,86 +1,45 @@
-import React, {useState, useEffect } from 'react';
-import { Button, View, Text, TouchableOpacity,StyleSheet, Image ,Picker} from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import image from './assets/virus.jpg';
-
-function HomeScreen({ navigation }) {
-  const [selectedValue, setSelectedValue] = useState("Argentina");
-  return (
-    <View style={ styles.container}>
-      <Text style={styles.title}> Información actualizada sobre COVID-19:</Text>
-      <Image 
-        source={image}
-        style={styles.image}
-      />
-      <Text style={{flex: 1, alignItems: 'center', justifyContent: 'center', padding: 50 }}> Seleccione el pais deseado:</Text>
-      
-        <Picker
-          selectedValue={selectedValue}
-          style={{ alignItems: 'center', justifyContent: 'center', height: 50, width: 200 }}
-          onValueChange={(itemValue, itemIndex) => setSelectedValue(itemValue)}
-        >
-          <Picker.Item label="Argentina" value="Argentina" />
-          <Picker.Item label="Afganistan" value="Afganistan" />
-          <Picker.Item label="Brasil" value="Brasil" />
-          
-        </Picker>
-        <Button
-        title="Go to Details..."
-        onPress={() => navigation.push('Details')}
-      />
-      <Button
-        title="Ir a otra Pagina"
-        onPress={() => navigation.navigate('Details')}
-      />
-    </View>
-  );
-}
-
-function DetailsScreen({ navigation }) {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Detalles del pais seleccionado:</Text>
-      <Button
-        title="Go to Details... again"
-        onPress={() => navigation.push('Details')}
-      />
-      <Button title="Go to Home" onPress={() => navigation.push('Inicio')} />
-      <Button title="Volver" onPress={() => navigation.goBack()} />
-    </View>
-  );
-}
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import Home from "./screems/Home";
+import Details from "./screems/Details";
 
 const Stack = createStackNavigator();
 
-function App() {
+function MyStack() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Inicio">
-        <Stack.Screen name="Inicio" component={HomeScreen} />
-        <Stack.Screen name="Details" component={DetailsScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={Home} />
+      <Stack.Screen name="Details" component={Details} />
+    </Stack.Navigator>
   );
-};
-const styles = StyleSheet.create({
-  container:{
-    flex:1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor:"#292929",
-  },
-  title: {fontSize: 20, color:"#fff"},
-  image: {height:200, width:200, borderRadius:100},
-  button:{
-    backgroundColor:"#a52a2a",
-    padding: 7,
-    marginTop:10,
-  },
-    buttonText:{
-      color:"#fff",
-      fontSize: 20,
-    },
-});
+}
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading:false,
+      country:[],
+      url:'https://api.covid19api.com/dayone/country/south-africa/status/confirmed'
+    }
+  }
+  componentDidMount(){
+    this.getCountry();
+  }
+  getCountry = () =>{
+    //this.setState({loading:true})
+    //fetch(this.state.url)
+
+  }
+
+  render() {
+    return (
+      <NavigationContainer>
+        <MyStack />
+      </NavigationContainer>
+    );
+  }
+}
 
 export default App;
---
+++ miPrimeraApp/screems/Home.jsx
@@ -0,0 +1,65 @@
+import React, { useState } from "react";
+import { Button, View, Text, Image, Picker, StyleSheet } from "react-native";
+import image from "../assets/covid.gif";
+
+const Home = ({ navigation }) => {
+  const [selectedValue, setSelectedValue] = useState("");
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      alignItems: "center",
+      backgroundColor: "black",
+    },
+    title: { fontSize: 20, color: "#fff" },
+    image: { height: 200, width: 200 },
+    button: {
+      backgroundColor: "#a52a2a",
+      padding: 7,
+      marginTop: 10,
+    },
+    buttonText: {
+      color: "#fff",
+      fontSize: 20,
+    },
+    piker: {
+      alignItems: "center",
+      justifyContent: "center",
+      height: 50,
+      width: 200,
+      color: "#fff",
+      backgroundColor: "#fff",
+    },
+    text: {
+      color: "#fff",
+      flex: 1,
+      alignItems: "center",
+      justifyContent: "center",
+      padding: 50,
+    },
+  });
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>COVID-19 information:</Text>
+      <Image source={image} style={styles.image} />
+      <Text style={styles.text}>Select the desired country:</Text>
+
+      <Picker
+        selectedValue={selectedValue}
+        style={styles.piker}
+        onValueChange={(itemValue, itemIndex) => setSelectedValue(itemValue)}
+      >
+        <Picker.Item label="Argentina" value="Argentina" />
+        <Picker.Item label="Afganistan" value="Afganistan" />
+        <Picker.Item label="Brasil" value="Brasil" />
+      </Picker>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate("Details")}
+      />
+    </View>
+  );
+};
+
+export default Home;
