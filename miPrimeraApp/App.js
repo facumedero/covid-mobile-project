5e7ea06a2958b982d88d3eae27f2544163606344
Facundo <facundomedero26@gmail.com>
2021-04-11 17:14:28 -0300

    consulta GET COUNTRYS funcionando, falta setear el resultado en la variable

--- miPrimeraApp/App.js
+++ miPrimeraApp/App.js
@@ -24,12 +24,32 @@ class App extends React.Component {
       url:'https://api.covid19api.com/dayone/country/south-africa/status/confirmed'
     }
   }
+  
+
   componentDidMount(){
     this.getCountry();
   }
   getCountry = () =>{
+    //consulta con todos los paises formato:
+    // {
+    //  "Country": "Canada",
+    //  "Slug": "canada",
+    //  "ISO2": "CA"
+    // },
+    var requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    };
+    
+    fetch("https://api.covid19api.com/countries", requestOptions)
+      .then(response => response.text())
+      .then(result =>console.log(result))
+      .catch(error => console.log('error', error))
+  
+    
+    
     //this.setState({loading:true})
-    //fetch(this.state.url)
+    //console.log(fetch(this.state.url))
 
   }
 
