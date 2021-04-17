09470f385b20888093aa9b4d2b3beac8a0b488ac
Matias Ezequiel Otero <matias.e.otero@accenture.com>
2021-04-16 22:04:38 -0300

    Add node modules in git ignore and delete it

--- App.js
+++ App.js
@@ -3,7 +3,6 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import Home from "./screens/Home";
 import Details from "./screens/Details";
-
 const Stack = createStackNavigator();
 
 function MyStack() {
@@ -14,53 +13,12 @@ function MyStack() {
     </Stack.Navigator>
   );
 }
-
-
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      loading:false,
-      country:[],
-      url:'https://api.covid19api.com/countries'
-    }
-  }
-
-  componentDidMount(){
-    fetch("https://api.covid19api.com/countries")
-      .then(res => res.json())
-      .then(
-        (result) => {
-          this.setState({
-            isLoaded: true,
-            country: result
-          });
-        },
-        (error) => {
-          this.setState({
-            isLoaded: true,
-            error
-          });
-        }
-      )
-  }
-
-  render() {
-    const { error, isLoaded, items } = this.state;
-    if (error) {
-      //return <div>Error: {error.message}</div>;
-    } else if (!isLoaded) {
-      return <div>Loading ...</div>;
-    } else {
-      return (
-        <NavigationContainer>
-        <MyStack/>
-      </NavigationContainer>
-      );
-    }
-  }
-}
-
-
+const App = ({}) => {
+  return (
+    <NavigationContainer>
+      <MyStack />
+    </NavigationContainer>
+  );
+};
 
 export default App;
